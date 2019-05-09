48592e234bcdbcaff36b8168905e1a7f2de96632
Nicolò Zannini <nikisa97@gmail.com>
2019-05-09 11:27:05 +0200

    Menu Scene created (not finished)

---
+++ Assets/Scripts + Plugin/Scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    
+    public void NewGame()
+    {
+        Debug.Log("NEW GAME");
+    }
+    public void LoadGame()
+    {
+        Debug.Log("LOAD GAME");
+    }
+    public void LevelSelection()
+    {
+        Debug.Log("LEVEL SELECTION");
+    }
+    public void Option()
+    {
+        Debug.Log("OPTION");
+    }
+    public void Exit()
+    {
+        Debug.Log("EXIT");
+    }
+
+}
